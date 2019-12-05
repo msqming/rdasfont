c6642ffb04f90b22434fe4b117adaa89dd13eb9e
msqming <msqming@163.com>
2019-12-05 17:57:22 +0800

    aabbc

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
 
-var router = new Router({
-  mode:'hash',
+const router = new Router({
+  mode:'history',
   routes:[{
     path: '/',
     redirect: '/Login',
